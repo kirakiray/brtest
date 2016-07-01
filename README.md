f6edf816de8c7bbe43ea658bdd7914d31e0e2201
huangyao <huangyao@myzaker.com>
2016-07-01 23:44:22 +0800

    upload the source

--- README.md
+++ README.md
@@ -1,151 +1,111 @@
-# brtest
+# srtest 2.0
 
-为BrowserRequire开发的单元测试工具
+It is a modular test framework for SugarRequire
+
+写给 SugarRequire 用的测试框架。
+
+## 关于srtest
+
+由于BrowserRequire升级2.0版本改名为SugarRequire，所以为它做的测试框架brtest升级也要更名srtest啦哈哈。
+
+当然不止更名，相关的使用方法也会变得更加自由更方便，抛弃brtest的callback形式的设定，才用面向对象的方式设定测试程序，更加直接方便。
+
+框架依赖jquery，以后会有剥离jquery的版本。
 
 ## 已确认兼容性
 
 移动端全线兼容；
 
-winPC >=IE8(best:>=ie10)
+winPC >=IE8，ie8及以下版本需要依赖jquery-1.9.1；
 
 其他全线兼容
 
 ## 使用方法
 
-### 1.引入依赖文件
+### 引入依赖文件
 
 ```html
-<link rel="stylesheet" type="text/css" href="brtest.css" />
+<link rel="stylesheet" type="text/css" href="srtest.css" />
+<script src="jquery-2.2.0.js"></script>
 <script src="brtest.js"></script>
 ```
 
-### 2.添加测试组
+创建一个测试组视图的方法，arguments[0] 就是组标题
 
-#### 2.1 对等测试
 ```javascript
-brtest.test('对等测试标题',function(con){
-    con.equal('对等测试1',1,1);
-    //因为 1 == 1 为true，显示为绿色
-    
-    con.equal('对等测试2',1,2);
-    //因为 1 == 2 为false，显示为红色
-    
-    con.more('匹配测试',1,[1,2,3]);
-    //因为 1 与后面数组内的某个值（1）相等，所以显示绿色
-    
-    con.more('匹配测试2',1,[4,2,3]);
-    //因为 1 与后面数组内都不相等，所以显示红色
+//用于创建测试组
+var g1 = srtest.group('我是第一组');
+
+//打印
+g1.log('我是g1 普通 信息');
+g1.warn('我是g1 警告性 信息');
+g1.error('我是g1 错误 信息');
+g1.succeed('我是g1 成功 信息');
+```
+
+### 重建信息
+
+用于修改已经打印的信息。
+
+```javascript
+var log1 = g1.warn('我还没执行');
+
+//一秒后改变状态
+setTimeout(function(){
+    log1.succeed('我执行成功了');
+},1000);
+```
+
+### 执行次数记录测试
+
+次数测试常用来记录事件的触发次数，异步函数的执行次数；
+
+通过orderTest 可以制定数目测试；
+
+```javascript
+g1.setOrder({
+    name : "test1",
+    count : 1
 });
 ```
 
-如果对等测试内容为object，在全等的情况下才会显示绿色；如果是克隆对象，则会显示黄色警告状；
-
-#### 2.2 顺序测试
+在设定之后，通过order来触发
 
 ```javascript
-brtest.orderTest('顺序测试标题',function(con){
-    //设定顺序
-    con.set('one');
-    con.set('two');
-    con.set('three');
+g1.order('test1');
+```
 
-    con.order('one');
-    //one属于第一级，显示绿色
-    
-    con.order('three');
-    //three属于第三级，在one（第一级）后面，所以显示绿色
-    
-    con.order('two');
-    //two属于第二级，因为第三级已经触发，所以显示红色
+上面案例设定了 name为test1的命令，而且test1必须**只**触发 1 次才算正确（安全提示）；不足1次则警告状态，超过1次则出现错误状态；（因为执行0次有可能会正确，但是超过1次是绝对错误）
+
+当然，我们也能设定次数区间，方便记录次数。
+
+```javascript
+g1.setOrder({
+    name : "test1",
+    descript : "我是描述出来的文字",
+    max : 3,
+    min : 1
 });
 ```
 
-顺序测试还能设置触发次数限制，count可设置触发固定次数 
+这个案例在执行1次以下是警告状态；1至3次内是安全提示；超过3次是错误提示；
+
+### 执行顺序和次数记录测试
+
+顺序测试常用于测试事件的触发先后顺序，异步函数的执行顺序，也会伴随着次数的记录；
+
+通过orderTest也可以制定顺序规则；
 
 ```javascript
-brtest.orderTest('顺序测试标题2',function(con){
-    //设定顺序
-    con.count('one',2);
-    //设置one必须触发两次
-
-    con.order('one');
-    //第一次触发one，次数不足两次，显示为警告黄色
-    
-    con.order('one');
-    //第二次触发one，次数两次显示为绿色
-    
-    con.order('one');
-    //第三次触发one，次数超过两次显示为红色
-    
-    con.count('two',2,4);
-    //设置two触发2到4次
-    
-    con.order('two');
-    //第一次触发two为警告黄色 
-    
-    con.order('two');
-    //第二次触发two为满足次数，显示绿色     
-    
-    con.order('two');
-    //第三次触发two为满足次数，显示绿色 
-    
-    con.count('three',2,true);
-    //设置three触发次数大于等于2次
-    
-    con.order('three');
-    //第一次触发three为未满次数，显示黄色  
-    
-    con.order('three');
-    //第二次触发three为满足次数，显示绿色 
-    
-    con.order('three');
-    //第三次触发three为满足次数，显示绿色 
+g1.setOrder({
+    name : "test1",
+    count : 1
+},{
+    name : "test2",
+    count : 1
 });
 ```
 
-顺序测试还能设置同等触发权限
+上面案例设定了name为test1和test2的命令，当test1被执行1次后，在执行1次test2才能安全提示；
 
-```javascript
-brtest.orderTest('顺序测试标题3',function(con){
-    //设定顺序
-    con.set('one').set('two');
-    con.set('three');
-
-    con.order('two');
-    //two与one属于同一级触发顺序，所以为绿色
-    
-    con.order('one');
-    //two与one属于同一级触发顺序，所以为绿色
-    
-    con.order('two');
-    //two与one属于同一级触发顺序，所以为绿色
-    
-    con.order('three');
-    //three属于第二级触发顺序，所以为绿色
-});
-```
-
-关于细致的特性，可以查看 brtestExample.html 测试文件
-
-#### 2.3 顺序测试（全局）
-
-顺序测试可以进行全局触发
-
-```javascript
-brtest.orderTest('orderTest1',function(con){
-    //设定顺序
-    con.set('one')
-    con.set('two');
-    con.set('three');
-});
-
-brtest.order('orderTest1',"one");
-//one属于第一级，显示绿色
-
-brtest.order('orderTest1',"three");
-//three属于第三级，在one（第一级）后面，所以显示绿色
-
-brtest.order('orderTest1',"two");
-//two属于第二级，因为第三级已经触发，所以显示红色
-```
-
+当然也能将`count`替换`max`和`min`；
---
+++ src/srtest.js
@@ -0,0 +1,340 @@
+(function(glo) {
+    //function
+    var getCallLine = function() {
+        try {
+            fuckyou();
+        } catch (e) {
+            if (!e.stack) {
+                return null;
+            }
+            var stackarr = e.stack.toString().split(/\n/g).filter(function(e) {
+                if (e) {
+                    return e;
+                }
+            });
+            return stackarr.slice(-1)[0].match(/\w+?:\/\/\/.+\d/)[0];
+        }
+    };
+
+    //class
+    //组容器
+    var SrGroup = function(title) {
+        this.ele = $('<div class="br_test_group"></div>');
+        this.title = $('<div class="br_test_g_title"><div class="br_test_g_title_content">' + title + '</div><div class="br_test_g_title_tips"><div class="total_content">0</div><div class="safe_content">0</div><div class="warn_content">0</div><div class="error_content">0</div></div></div>');
+        this.ele.append(this.title);
+        this.content = $('<div class="br_test_g_content"></div>');
+        this.ele.append(this.content);
+        this.totalCount = 0;
+        this.safeCount = 0;
+        this.warnCount = 0;
+        this.errorCount = 0;
+    };
+    SrGroup.fn = SrGroup.prototype;
+    SrGroup.fn.changeTotal = function(num) {
+        this.ele.find('.total_content').text(num);
+    };
+    SrGroup.fn.changeSafe = function(num) {
+        this.ele.find('.safe_content').text(num);
+    };
+    SrGroup.fn.changeWarn = function(num) {
+        this.ele.find('.warn_content').text(num);
+    };
+    SrGroup.fn.changeError = function(num) {
+        this.ele.find('.error_content').text(num);
+    };
+    SrGroup.fn._appendLine = function() {
+        var srline = new SrLine();
+        var _this = this;
+        srline.onPrint = function(e) {
+            if (e.beforeState == e.nowState) {
+                return;
+            }
+            switch (e.beforeState) {
+                case "warn":
+                    _this.changeWarn(--_this.warnCount);
+                    break;
+                case "error":
+                    _this.changeError(--_this.errorCount);
+                    break;
+                case "succeed":
+                    _this.changeSafe(--_this.safeCount);
+                    break;
+            }
+            switch (e.nowState) {
+                case "warn":
+                    _this.changeWarn(++_this.warnCount);
+                    break;
+                case "error":
+                    _this.changeError(++_this.errorCount);
+                    break;
+                case "succeed":
+                    _this.changeSafe(++_this.safeCount);
+                    break;
+            }
+        };
+        this.content.append(srline.ele);
+        return srline;
+    };
+    SrGroup.fn.log = function(text) {
+        var srline = this._appendLine();
+        srline.log(text);
+        return srline;
+    };
+    SrGroup.fn.warn = function(text) {
+        var srline = this._appendLine();
+        srline.warn(text);
+        return srline;
+    };
+    SrGroup.fn.error = function(text) {
+        var srline = this._appendLine();
+        srline.error(text);
+        return srline;
+    };
+    SrGroup.fn.succeed = function(text) {
+        var srline = this._appendLine();
+        srline.succeed(text);
+        return srline;
+    };
+    //设置序列记录器
+    SrGroup.fn.setOrder = function() {
+        // var data = {
+        //     name: "",
+        //     descript: "",
+        //     count: 1,
+        //     max: "",
+        //     min: ""
+        // };
+        var orderMap = this._orderMap || (this._orderMap = {});
+        var _this = this;
+
+        //当前开始的id
+        this._orderId = 0;
+        //order的总数记录
+        this._groupCount = 0;
+        $.each(arguments, function(i, e) {
+            var logObj = _this.warn(e.descript || e.name);
+            var orderObj = {
+                id: i,
+                logObj: logObj,
+                max: e.max || e.count || 1,
+                min: e.min || e.count || 1,
+                count: 0
+            };
+            orderMap[e.name] = orderObj;
+            logObj.inContent.addText([{
+                k: "max",
+                v: orderObj.max
+            }, {
+                k: "min",
+                v: orderObj.min
+            }]);
+        });
+        //只能执行一次
+        this.setOrder = function() {
+            console.warn('setOrder invalid!');
+        };
+    };
+    SrGroup.fn.order = function(name) {
+        var orderObj = this._orderMap[name];
+
+        //顺序计算
+        if (orderObj.id > this._orderId) {
+            //当大于id时，更换当前id顺序
+            this._orderId = orderObj.id;
+        }
+
+        //数量计算
+        orderObj.count++;
+        this._groupCount++;
+
+        // 在最小值内是警告状态   在范围内是正确状态    超出范围是错误状态
+        var logarr = [{
+            k: "id",
+            v: orderObj.count
+        }, {
+            k: "groupId",
+            v: this._groupCount
+        }];
+
+        if (this._orderId > orderObj.id) {
+            //小于id属于出错
+            logarr.push({
+                type: 'error',
+                k: "info",
+                v: "sequence error"
+            });
+            orderObj.logObj.error(logarr);
+        } else if (orderObj.count > orderObj.max) {
+            logarr.push({
+                type: 'error',
+                k: "info",
+                v: "exceed the maximum limit"
+            });
+            orderObj.logObj.error(logarr);
+        } else if (orderObj.count < orderObj.min) {
+            orderObj.logObj.warn(logarr);
+        } else {
+            orderObj.logObj.succeed(logarr);
+        }
+    };
+
+    //行容器
+    var SrLine = function() {
+        var ele = this.ele = $('<div class="br_test_g_line"><span class="br_test_g_line_text"></span><span class="more_clicker">more</span></div>');
+        var con = this.inContent = new SrLineInner();
+        ele.append(this.inContent.ele);
+        //点击更多按钮
+        ele.find('.more_clicker').click(function() {
+            var $this = $(this);
+            if ($this.hasClass('more_clicker_shou')) {
+                $this.removeClass('more_clicker_shou');
+                con.hide();
+            } else {
+                $this.addClass('more_clicker_shou');
+                con.show();
+            }
+        });
+    };
+    SrLine.fn = SrLine.prototype;
+    SrLine.fn.log = function(text) {
+        this.ele.removeClass('br_test_warn').removeClass('br_test_safe').removeClass('br_test_error');
+        this._emitPrint('log', text);
+    };
+    SrLine.fn.warn = function(text) {
+        this.ele.removeClass('br_test_safe').removeClass('br_test_error');
+        this.ele.addClass('br_test_warn');
+        this._emitPrint('warn', text);
+    };
+    SrLine.fn.error = function(text) {
+        this.ele.removeClass('br_test_warn').removeClass('br_test_safe');
+        this.ele.addClass('br_test_error');
+        this._emitPrint('error', text);
+    };
+    SrLine.fn.succeed = function(text) {
+        this.ele.removeClass('br_test_warn').removeClass('br_test_error');
+        this.ele.addClass('br_test_safe');
+        this._emitPrint('succeed', text);
+    };
+    SrLine.fn._emitPrint = function(state, text) {
+        var textObj = [{
+            type: state,
+            k: "state",
+            v: state
+        }]
+        if ((typeof text).toLowerCase() == 'string') {
+            this.ele.find('.br_test_g_line_text').text(text);
+            text && textObj.push({
+                k: "text",
+                v: text
+            });
+        }
+        if ((typeof text).toLowerCase() == 'object') {
+            textObj = textObj.concat(text);
+        }
+        var callline = getCallLine();
+        textObj.push({
+            type: "console",
+            k: "position",
+            v: callline,
+            click: function() {
+                console.log(callline);
+            }
+        });
+        this.inContent.addText(textObj);
+
+        var beforeState = this._state;
+        this._state = state;
+        this.onPrint({
+            text: text,
+            beforeState: beforeState,
+            nowState: state
+        });
+    };
+    //改变状态触发的callback
+    SrLine.fn.onPrint = function(data) {
+        console.log(data);
+    };
+
+    //行内部容器
+    var SrLineInner = function() {
+        this.ele = $('<div class="_gline_incontent _shou"></div>');
+    };
+    SrLineInner.fn = SrLineInner.prototype;
+    SrLineInner.fn.show = function() {
+        this.ele.removeClass('_shou');
+    };
+    SrLineInner.fn.hide = function() {
+        this.ele.addClass('_shou');
+    };
+    SrLineInner.fn.addText = function(datas) {
+        // var datas = [{
+        //     type: "error",
+        //     k: "",
+        //     v: "",
+        //     click:""
+        // }];
+        var _gline_incontent_block = $('<div class="_gline_incontent_block"></div>');
+        $.each(datas, function(i, e) {
+            var _gline_incontent_line = $('<div class="_gline_incontent_line"></div>');
+
+            //根据类型设定
+            switch (e.type) {
+                case "warn":
+                    _gline_incontent_line.addClass('_incontent_line_warn');
+                    break;
+                case "error":
+                    _gline_incontent_line.addClass('_incontent_line_err');
+                    break;
+                case "succeed":
+                    _gline_incontent_line.addClass('_incontent_line_safe');
+                    break;
+                case "console":
+                    _gline_incontent_line.addClass('_canclick');
+                    break;
+            }
+
+            //设定值
+            _gline_incontent_line.append('<div class="_gline_incontent_key">' + e.k + '</div>');
+            _gline_incontent_line.append('<div class="_gline_incontent_value">' + e.v + '</div>');
+
+            //设置点击事件
+            if (e.click) {
+                _gline_incontent_line.find('._gline_incontent_value').click(e.click);
+            }
+
+            _gline_incontent_block.append(_gline_incontent_line);
+        });
+        this.ele.append(_gline_incontent_block);
+    };
+
+    //base
+    //主体容器
+    var $srall = $('<div class="br_test_all"><div class="br_test_title"></div></div>');
+    var $container = $('<div class="br_test_container"></div>');
+    $srall.append($container);
+
+    //main
+    function srInit(options) {
+        var defaults = {
+            title: "SRTEST"
+        };
+        $.extend(defaults, options);
+        $srall.find('.br_test_title').text(defaults.title);
+        $('body').append($srall);
+    };
+
+    function group(title) {
+        var srgroup = new SrGroup(title);
+        $container.append(srgroup.ele);
+        return srgroup;
+    }
+
+    //init
+
+    //global
+    glo.srtest = {
+        init: srInit,
+        group: group
+    };
+
+})(window);
